4c2cea2c0f90fa373c0e64b492311c94db753143
Eric Guliev-Datsin <ericgd@protonmail.com>
2025-03-10 00:59:12 -0700

    http: Get absolute uri from http request

--- src/http_server/mod.rs
+++ src/http_server/mod.rs
@@ -2,6 +2,7 @@ mod config;
 
 use http_body_util::Full;
 use hyper::body::Bytes;
+
 use hyper::server::conn::http1;
 use hyper::service::service_fn;
 use hyper::{Request, Response};
@@ -12,16 +13,61 @@ use tokio::task;
 
 const CONFIG_PATH: &str = "config_http.json";
 
-async fn const_response(
-    _: Request<hyper::body::Incoming>,
-    request_path: &str,
-) -> Result<Response<Full<Bytes>>, Box<dyn std::error::Error + Send + Sync>> {
-    let response_data = fs::read(request_path).await?;
-    let body: Bytes = response_data.into();
+pub fn extract_full_url<T>(req: &Request<T>) -> Result<Bytes, &str> {
+    // Get the URI from the request
+    let uri = req.uri();
 
+    // Create a BytesMut to build our URL
+    let mut url = Vec::new();
+
+    // Add scheme (http/https)
+    if let Some(scheme) = uri.scheme() {
+        url.extend_from_slice(scheme.as_str().as_bytes());
+        url.extend_from_slice(b"://");
+    } else {
+        // Default to http if scheme is not present
+        url.extend_from_slice(b"http://");
+    }
+
+    // Add authority (host:port)
+    if let Some(authority) = uri.authority() {
+        url.extend_from_slice(authority.as_str().as_bytes());
+    } else {
+        // Try to get host from headers if not in URI  (common in HTTP/1.1)
+        let host = req
+            .headers()
+            .get(hyper::header::HOST)
+            .ok_or("Failed to find host")?;
+        url.extend_from_slice(host.as_bytes());
+    }
+
+    // Add path
+    url.extend_from_slice(uri.path().as_bytes());
+
+    // Add query parameters if present
+    if let Some(query) = uri.query() {
+        url.extend_from_slice(b"?");
+        url.extend_from_slice(query.as_bytes());
+    }
+
+    // Convert to Bytes and return
+    Ok(url.into())
+}
+
+async fn proxy_response(
+    req: Request<hyper::body::Incoming>,
+    inject_payload_path: &str,
+) -> Result<Response<Full<Bytes>>, Box<dyn std::error::Error + Send + Sync>> {
+    let _inject_payload = fs::read(inject_payload_path);
+    let full_url = extract_full_url(&req)?;
+
+    // let response: Vec<u8> = todo!("Forward HTTP Request, send response with injected payload");
+
+    // response.extend(inject_payload.await?);
+    // let body: Bytes = response.into();
     let response = Response::builder()
         .header("Content-Type", "text/html")
-        .body(Full::new(body))?;
+        .body(Full::new(full_url))?;
 
     Ok(response)
 }
@@ -31,6 +77,7 @@ pub async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     let cfg = config::read_config(CONFIG_PATH);
 
     let listener = TcpListener::bind(cfg.listen_address).await?;
+    log::info!("HTTP Server listening on {}", cfg.listen_address);
 
     loop {
         let (stream, addr) = listener.accept().await?;
@@ -43,7 +90,7 @@ pub async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
         task::spawn(async move {
             if let Err(err) = http1::Builder::new()
                 // `service_fn` converts our function in a `Service`
-                .serve_connection(io, service_fn(|req| const_response(req, &request_path)))
+                .serve_connection(io, service_fn(|req| proxy_response(req, &request_path)))
                 .await
             {
                 eprintln!("Error serving connection: {:?}", err);
